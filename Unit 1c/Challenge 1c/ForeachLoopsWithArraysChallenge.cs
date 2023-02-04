c5ff6bfa2cd8afb03530b72d6c2a107e54e34b10
Ronin Thatcher <122494624+roninthatcher@users.noreply.github.com>
2023-02-03 19:35:07 -0700

    Update ForeachLoopsWithArraysChallenge.cs

--- Unit 1c/Challenge 1c/ForeachLoopsWithArraysChallenge.cs
+++ Unit 1c/Challenge 1c/ForeachLoopsWithArraysChallenge.cs
@@ -1,1 +1,25 @@
 
+using System;
+
+namespace MyApplication
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("What would you like to craft?");
+      string[] materials = {"Wood", ">Stone", "Iron", "Charm"};
+      foreach (string i in materials) 
+      {
+        Console.WriteLine(i);
+      } 
+      
+      Console.WriteLine("What you could make:");
+      string[] recipes = {"Stone Axe (Wood + Stone)", "Magic Stone (Enchantment + Stone", "Iron Spell Caster (Iron + Enchantment)"};
+      foreach (string i in recipes) 
+      {
+        Console.WriteLine(i);
+      } 
+    }
+  }
+}
